a1c3b1b614f88bd24fc1a35f425ff7816c04310f
Francisco García <g.garciafran201@gmail.com>
2025-06-30 16:08:45 +0200

    Endpoints de project

--- src/api/routes/project.py
+++ src/api/routes/project.py
@@ -8,8 +8,42 @@ def get_all_projects():
     projects = Project.query.all()
     return jsonify([p.serialize() for p in projects]), 200
 
-@project_api.route('/projects/<int:id>' methods=['GET'])
+@project_api.route('/projects/<int:id>', methods=['GET'])
 def get_project(id):
     project = Project.query.get_or_404(id)
     return jsonify(project.serialize()), 200
 
+@project_api.route('/projects', methods=['POST'])
+def create_project():
+    data = request.get.json()
+    project = Project(
+        title = data['title'],
+        description = data['description'],
+        image_url = data.get('image_url'),
+        hashtags = data.get('hashtags'),
+        owner_id = data['owner_id']
+    )
+    db.session.add(project)
+    db.session.commit()
+    return jsonify(project.serialize()), 200
+
+@project_api.route('/projects/<int:id>', methods=['PUT'])
+def update_project(id):
+    project = Project.query.get_or_404(id)
+    data = request.get_json()
+    
+    project.title = data.get('title',project.title)
+    project.description = data.get('description', project.description)
+    project.image_url = data.get('image_url', project.image_url)
+    project.hashtags = data.get('hashtags', project.hashtags)
+    project.is_accepting_applications = data.get('is_accepting_applications', project.is_accepting_applications)
+    
+    db.session.commit()
+    return jsonify(project.serialize()), 200
+
+@project_api.route('/projects(<int:id>)', methods=['DELETE'])
+def delete_project(id):
+    project = Project.query.get_or_404(id)
+    db.session.delete(project)
+    db.session.commit()
+    return '', 204
