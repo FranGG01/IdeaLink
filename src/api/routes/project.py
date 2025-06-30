d8f799ec55a4f133c64c05e2b690b742f6bb8cc0
Francisco García <g.garciafran201@gmail.com>
2025-06-30 12:08:13 +0200

    arreglo de back

--- src/api/routes/project.py
+++ src/api/routes/project.py
@@ -8,7 +8,7 @@ def get_all_projects():
     projects = Project.query.all()
     return jsonify([p.serialize() for p in projects]), 200
 
-@project_api.route('/projects/<int:id>' methods=['GET'])
+@project_api.route('/projects/<int:id>', methods=['GET'])
 def get_project(id):
     project = Project.query.get_or_404(id)
     return jsonify(project.serialize()), 200
