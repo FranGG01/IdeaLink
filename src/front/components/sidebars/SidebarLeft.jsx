6dc1aa24bc24f24ec01dceb93a9e7a46c0a3f1fe
Francisco García <g.garciafran201@gmail.com>
2025-07-02 00:28:57 +0200

    Se crean rutas en el feed

--- src/front/components/sidebars/SidebarLeft.jsx
+++ src/front/components/sidebars/SidebarLeft.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Home, Lightbulb, Search, Folder, User } from "lucide-react";
 import Separator from "./Separator";
+import { Link } from "react-router-dom";
 
 export default function SidebarLeft() {
   const [activeTab, setActiveTab] = useState("Dashboard");
@@ -11,13 +12,16 @@ export default function SidebarLeft() {
     { label: "Explorar", icon: Search },
     { label: "Proyectos", icon: Folder },
     { label: "Perfil", icon: User },
+    { label: "Sobre Nosotros", icon: User, to: "/about_us" },
+    { label: "Mensajes", icon: User, to: "/chat" },
   ];
 
   return (
     <aside className="w-64 bg-[#1e293b] p-6 space-y-6 text-white">
       <nav className="space-y-3">
-        {navItems.map(({ label, icon: Icon }) => (
-          <button
+        {navItems.map(({ label, icon: Icon, to }) => (
+          <Link
+            to={to}
             key={label}
             className={`w-full flex items-center space-x-3 text-left p-2 rounded-md transition-colors duration-200 cursor-pointer ${activeTab === label ? "bg-blue-600" : "hover:bg-blue-500/20"
               }`}
@@ -25,7 +29,7 @@ export default function SidebarLeft() {
           >
             <Icon className="w-4 h-4" />
             <span>{label}</span>
-          </button>
+          </Link>
         ))}
       </nav>
 
