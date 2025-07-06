3630605fedfb1d34d345e12d03fd4138513f70e3
juanca <oohankoo@hotmail.com>
2025-07-06 19:52:55 +0200

    cambio

--- src/front/components/feed_central/Tarjeta_feed.jsx
+++ src/front/components/feed_central/Tarjeta_feed.jsx
@@ -3,6 +3,7 @@ import './Feed_central.css'
 import { Avatar } from "@material-tailwind/react";
 import Modal_postularse from './Modal_postularse';
 import Modal_leer_mas from './Modal_leer_mas';
+import { Card, CardHeader, CardBody, CardFooter, Typography } from "@material-tailwind/react";
 
 
 export default function Tarjeta({ project }) {
@@ -41,11 +42,11 @@ export default function Tarjeta({ project }) {
                 )}
             </CardBody>
 
-            </Card.Body>
-            <Card.Footer className='flex justify-end p-1 gap-2'>
+
+            <CardFooter className='flex justify-end p-1 gap-2'>
                 <Modal_leer_mas />
                 <Modal_postularse />
-            </Card.Footer>
-        </Card >
+            </CardFooter>
+        </Card>
     );
 }
