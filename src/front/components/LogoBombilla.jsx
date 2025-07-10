21aa251c525575d3b4886868cf9c2ca9794e8dee
juanca <oohankoo@hotmail.com>
2025-07-10 20:33:12 +0200

    centrado logo y cambios botones solicitud

--- src/front/components/LogoBombilla.jsx
+++ src/front/components/LogoBombilla.jsx
@@ -38,7 +38,7 @@ export default function LogoBombilla() {
                         key="bombilla"
                         src={Bombilla}
                         alt="Logo"
-                        className="w-full h-full object-contain"
+                        className="w-[200px] h-[150px] object-cover pb-22 "
                         initial={{ scale: 1 }}
                         animate={{ scale: 1 }}
                         exit={{ scale: 0, opacity: 0, rotate: 90 }}
