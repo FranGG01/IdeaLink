47ace99c8c7791c83be096aded9889682dab94ac
Francisco García <g.garciafran201@gmail.com>
2025-07-14 20:19:19 +0200

    Se comienza boton postularse

--- src/api/routes/application.py
+++ src/api/routes/application.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from api.models import db, Postularse
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 application_api = Blueprint('application_api', __name__)
 
@@ -8,7 +9,7 @@ def get_all_applications():
     applications = Postularse.query.all()
     return jsonify([a.serialize() for a in applications]), 200
 
-@application_api.route('/application/<int:project_id>)', methods=['GET'])
+@application_api.route('/application/<int:project_id>', methods=['GET'])
 def get_applications_for_project(project_id):
     applications = Postularse.query.filter_by(project_id=project_id).all()
     return jsonify([a.serialize() for a in applications]), 200
@@ -16,6 +17,8 @@ def get_applications_for_project(project_id):
 @application_api.route('/applications', methods=['POST'])
 def apply_to_project():
     data = request.get_json()
+    user_id = get_jwt_identity() 
+    
     application = Postularse(
         message = data.get('message'),
         user_id = data['user_id'],
