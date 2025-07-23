cbee0135b281c475f25567a117e488eb70685880
juan camilo <oohankoo@hotmail.com>
2025-07-23 14:51:37 +0200

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -2,9 +2,15 @@
 # exit on error
 set -o errexit
 
+# Frontend
 npm install
 npm run build
 
+# Backend
 pipenv install
 
-pipenv run upgrade
+# Alembic: marca migraciones como aplicadas (evita errores si la DB ya tiene las tablas)
+pipenv run alembic stamp head
+
+# Si necesitas aplicar migraciones nuevas (opcionales si ya están aplicadas)
+# pipenv run flask db upgrade
