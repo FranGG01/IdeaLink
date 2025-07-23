1730223baf270461d3ec45ba2cebe36d016f7ef7
juan camilo <oohankoo@hotmail.com>
2025-07-23 13:01:32 +0000

    update

--- render_build.sh
+++ render_build.sh
@@ -9,8 +9,4 @@ npm run build
 # Backend
 pipenv install
 
-# Alembic: marca migraciones como aplicadas (evita errores si la DB ya tiene las tablas)
-pipenv run alembic stamp head
-
-# Si necesitas aplicar migraciones nuevas (opcionales si ya están aplicadas)
-# pipenv run flask db upgrade
+pipenv run alembic upgrade head
