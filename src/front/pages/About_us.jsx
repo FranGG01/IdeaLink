b88248cd1c71893c991d4e30f4b8c3d6b8ac539c
juan camilo <oohankoo@hotmail.com>
2025-07-28 09:05:50 +0000

    update

--- src/front/pages/About_us.jsx
+++ src/front/pages/About_us.jsx
@@ -19,7 +19,7 @@ const team = [
         role: "Full Stack Developer",
         description:
             "Disfruto convertir ideas en productos funcionales y bien ejecutados. Me centro en la calidad, los detalles y en que cada proyecto tenga un propósito claro y útil.",
-        image: "../public/juanka.png",
+        image: "/juanka.png",
         linkedin: "https://www.linkedin.com",
         github: "https://github.com/00hank00",
     },
